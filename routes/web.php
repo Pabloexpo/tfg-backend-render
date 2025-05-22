973445376b646d07a2b3675e59b8a0a5c40998ef
Pablo Expósito <pabloexpops3@gmail.com>
2025-05-22 12:54:55 +0200

    commit

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('/importar-sql', function () {
+    try {
+        DB::unprepared(file_get_contents(database_path('scripts/ct_padel_2025.sql')));
+        return '✅ Base de datos importada correctamente';
+    } catch (\Exception $e) {
+        return '❌ Error: ' . $e->getMessage();
+    }
 });
